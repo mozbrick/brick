aed2e7762673b3c3e7055ab4ba53599b188d1af5
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-07-02 15:16:43 -0700

    initial optionbar HTML/css implementation; does not does option addition/removal or icons yet

--- optionbar/src/optionbar.js
+++ optionbar/src/optionbar.js
@@ -2,12 +2,16 @@
     xtag.register('x-optionbar', {
         lifecycle: {
             created: function(){
-
                 this.options.forEach(function(option){
                     if(this.name){
                         option.name = this.name;
                     }
+                    if(this.group){
+                        option.group = this.group;
+                    }
                 }.bind(this));
+
+                console.log(this.childNodes);
             }
         },
         events: {
@@ -17,10 +21,13 @@
             "name": {
                 attribute: {}
             },
+            "group": {
+                attribute: {}
+            },
             "options": {
-                    get: function(){
-                        return xtag.queryChildren(this, "x-toggle");
-                    }
+                get: function(){
+                    return xtag.queryChildren(this, "x-toggle");
+                }
             }
         },
         methods: {
