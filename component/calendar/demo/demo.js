ffb87c9f903b54b5665b088ac46d2595313c4e1c
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-08-06 17:55:19 -0700

    add simple event-scheduler demo to x-calendar demos page

--- component/calendar/demo/demo.js
+++ component/calendar/demo/demo.js
@@ -43,10 +43,10 @@ function cleanHtmlSource(html, ignoreAttrs){
 }
 
 
-function updateHtmlMarkup(markupEl, calendar, isInit){
+function updateHtmlMarkup(markupEl, contextEl, isInit){
     // remove shadow dom internals
-    var markup = calendar.outerHTML.replace(calendar.innerHTML, ""); 
-    
+    var calendar = contextEl.querySelector("x-calendar");
+    var markup = contextEl.innerHTML.replace(calendar.innerHTML, "");
     markup = cleanHtmlSource(markup, ["active"])
 
     // special case to account for escaped json doublequote when 
@@ -71,6 +71,10 @@ function updateHtmlMarkup(markupEl, calendar, isInit){
 
 function getMarkupEl(demoSect){
     return demoSect.querySelector(".markup-wrap .html");
+}
+
+function getContextEl(demoSect){
+    return demoSect.querySelector(".demo");
 }
 
 function getCalendar(demoSect){
@@ -140,33 +144,101 @@ function initSimpleCustomRenderDemo(){
     };
 }
 
+function initEventSchedulerDemo(){
+    /** set up custom event handling demo using provided API hooks **/
+
+    // the dictionary of iso datestring keys mapped to event data
+    var DATE_INFO = {};
+    // the currently displayed date
+    var CURR_ISO = null;
+
+    // aliases for DOM manipulation
+    var eventsStage = document.getElementById("scheduler-demo");
+    var eventsCal = eventsStage.querySelector("x-calendar");
+    var eventsDateHeader = document.getElementById("scheduler-date");
+    var eventsInfo = document.getElementById("scheduler-info");
+    var eventsSaveButton = document.getElementById("scheduler-save");
+
+    // define a .customRenderFn that provides extra styling 
+    // information for day elements with stored data
+    eventsCal.customRenderFn = function(dayEl, date, isoStr){
+        if(isoStr === CURR_ISO){
+            xtag.addClass(dayEl, "scheduler-current");
+        }
+        else{
+            xtag.removeClass(dayEl, "scheduler-current");
+        }
+
+        if(isoStr in DATE_INFO){
+            dayEl.setAttribute("scheduler-has-info", true);
+        }
+        else{
+            dayEl.removeAttribute("scheduler-has-info");
+        }
+    }
+
+    // respond to calendar taps
+    eventsCal.addEventListener("datetap", function(e){
+        // grab date info from event as both a Date and a string
+        var date = e.detail.date;
+        var dateStr = e.detail.iso;
+        // get dictionary content for this date
+        var content = (dateStr && dateStr in DATE_INFO) ? 
+                                        DATE_INFO[dateStr] : "";
+        // set up text area                                                
+        eventsInfo.value = content;
+        eventsInfo.disabled = !dateStr;
+        eventsSaveButton.disabled = !dateStr;
+        // remember currently shown date
+        CURR_ISO = dateStr;
+        eventsDateHeader.textContent = (dateStr) ? dateStr : "None";
+        // programmatically toggle date object with .toggleDateOn
+        eventsCal.toggleDateOn(date);
+        // forces rerender of calendar
+        eventsCal.render();
+    });
+
+    // save button listener; simply adds textarea value to data
+    eventsSaveButton.addEventListener("click", function(e){
+        if(CURR_ISO){
+            if(eventsInfo.value){
+                DATE_INFO[CURR_ISO] = eventsInfo.value;
+            }
+            else{
+                delete DATE_INFO[CURR_ISO];
+            }
+        }
+        eventsCal.render();
+    });
+}
+
 document.addEventListener('DOMComponentsLoaded', function(){
     xtag.addEvent(document, "datetoggleon:delegate(.demo-wrap)", function(e){
         var demoSect = this;
-        var calendar = getCalendar(demoSect);
+        var contextEl = getContextEl(demoSect);
         var markupEl = getMarkupEl(demoSect);
-        updateHtmlMarkup(markupEl, calendar);
+        updateHtmlMarkup(markupEl, contextEl);
     });
 
     xtag.addEvent(document, "datetoggleoff:delegate(.demo-wrap)", function(e){
         var demoSect = this;
-        var calendar = getCalendar(demoSect);
+        var contextEl = getContextEl(demoSect);
         var markupEl = getMarkupEl(demoSect);
-        updateHtmlMarkup(markupEl, calendar);
+        updateHtmlMarkup(markupEl, contextEl);
     });
 
     xtag.addEvent(document, "datetap:delegate(.demo-wrap)", function(e){
         var demoSect = this;
-        var calendar = getCalendar(demoSect);
+        var contextEl = getContextEl(demoSect);
         var markupEl = getMarkupEl(demoSect);
-        updateHtmlMarkup(markupEl, calendar);
+        updateHtmlMarkup(markupEl, contextEl);
     });
 
 
     xtag.query(document, '.demo-wrap').forEach(function(demoSect){
-        var calendar = getCalendar(demoSect);
+        var contextEl = getContextEl(demoSect);
         var markupEl = getMarkupEl(demoSect);
-        updateHtmlMarkup(markupEl, calendar, false);
+        updateHtmlMarkup(markupEl, contextEl);
     });
 
 
@@ -193,5 +265,6 @@ document.addEventListener('DOMComponentsLoaded', function(){
     initFrenchCalendar();
     initEventsDemo();
     initSimpleCustomRenderDemo();
+    initEventSchedulerDemo();
     prettyPrint();
 });
