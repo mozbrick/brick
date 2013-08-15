b1a03cd7e77e9a5d3c5ebb6653886fcc4f6e5c18
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-08-15 16:52:21 -0700

    update download generator to account for nested/recursive dependency chains

--- build/download.js
+++ build/download.js
@@ -1,16 +1,38 @@
 
 var path = require('path');
 var site = require('./statictools');
+var _ = require('lodash');
 
 var avow = site.avow;
 var getJSON = site.getJSON;
 var each = site.each;
 var err = site.err;
 
-var buildDependencyTree = avow(function (fulfill, reject, list) {
+// return a set of all nodes reachable from the given key
+function getReachableSet(graph, startKey){
+  var seen = {};
+  function _traverseGraph(key){
+    if(key in seen || (!key in graph)){
+      return;
+    }
+    else{
+      seen[key] = true;
+      var neighbors = graph[key] || [];
+      neighbors.forEach(function(neighbor){
+        _traverseGraph(neighbor);
+      });
+    }
+  }
+  _traverseGraph(startKey);
+  return seen;
+}
+
+var buildDependencyGraph = avow(function (fulfill, reject, list) {
   console.log('building dependency tree');
-  var tree = {};
+  var initGraph = {};
   each(list, function(fulfill, reject, c) {
+    // first, build the graph of each component's immediate dependencies
+    // by reading each component's settings json file
     console.log(c);
     if (c === 'core') {
       fulfill();
@@ -18,11 +40,20 @@ var buildDependencyTree = avow(function (fulfill, reject, list) {
     }
     var docPath = path.join('component',c,'xtag.json');
     getJSON(docPath).then(function(json) {
-      tree[c] = json.dependencies || [];
+      initGraph[c] = json.dependencies || [];
       fulfill()
     }, err('could not load ' + docPath));
   }).then(function() {
-    fulfill(tree)
+    // then, flesh out the dependency graph by recursing through connections
+    // and getting the reachable set for each node
+    var dependencyGraph = {};
+    _.keys(initGraph).forEach(function(key){
+        // components omit self from dependencies, since this is redundant info
+        var dependencies = _.omit(getReachableSet(initGraph, key), key);
+        dependencyGraph[key] = _.keys(dependencies);
+    });
+    console.log("dependencies:\n", dependencyGraph);
+    fulfill(dependencyGraph);
   }, reject);
 });
 
@@ -34,7 +65,7 @@ var renderDownloadPage = avow(function (fulfill, reject, tree) {
 console.log('generating download page');
 
 getJSON('build/components.json')
-  .then(buildDependencyTree, err('could not fetch component list'))
+  .then(buildDependencyGraph, err('could not fetch component list'))
   .then(renderDownloadPage, err('could not get dependencies'))
   .then(false, err('problem'));
 
