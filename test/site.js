6e3726ccd0c44c58c9e52ec678f7e353398ed059
Matt Claypotch <thepotch@gmail.com>
2013-09-18 16:49:57 -0700

    add cwd to path

--- test/site.js
+++ test/site.js
@@ -1,7 +1,11 @@
 
 casper.test.begin('Brick homepage loads and has no JS errors.', 4, function suite(test) {
 
-    casper.start("http://mozilla.github.io/brick", function() {
+    var fs = require('fs');
+
+    rootPath = fs.workingDirectory;
+
+    casper.start(rootPath + "/index.html", function() {
         test.assertTitle("Brick", "brick homepage title is as expected");
         test.assertExists('x-flipbox', "x-flipbox demo is present");
         casper.viewport(320,480);
@@ -21,7 +25,6 @@ casper.test.begin('Brick homepage loads and has no JS errors.', 4, function suit
     casper.then(function() {
         test.assertExists('x-flipbox:not([flipped])', 'x-flipbox flips back properly');
     });
-
 
     casper.run(function() {
         test.done();
