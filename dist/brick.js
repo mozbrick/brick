aa643465e898a060b7313c82a3435dd39bc66c1d
Arron Schaar <aschaar@gmail.com>
2013-11-25 11:19:34 -0800

    removed shiftbox from 1.0 release

--- dist/brick.js
+++ dist/brick.js
@@ -3572,54 +3572,6 @@ if (typeof WeakMap === "undefined") {
 })();
 
 (function() {
-    xtag.register("x-shiftbox", {
-        lifecycle: {
-            created: function() {
-                this.xtag.data.opened = this.hasAttribute("open");
-            }
-        },
-        events: {
-            transitionend: function(e) {
-                if (xtag.matchSelector(e.target, "x-shiftbox > section")) {
-                    if (this.hasAttribute("open") && !this.xtag.data.opened) {
-                        this.xtag.data.opened = true;
-                        xtag.fireEvent(this, "opened");
-                    } else if (!this.hasAttribute("open") && this.xtag.data.opened) {
-                        this.xtag.data.opened = false;
-                        xtag.fireEvent(this, "closed");
-                    }
-                }
-            }
-        },
-        accessors: {
-            shift: {
-                attribute: {},
-                get: function() {
-                    return this.getAttribute("shift") || "";
-                }
-            },
-            open: {
-                attribute: {},
-                set: function() {
-                    var asideWidth = getComputedStyle(xtag.query(this, "aside")[0]).width;
-                    this.setAttribute("data-asideSize", asideWidth);
-                }
-            }
-        },
-        methods: {
-            toggle: function() {
-                console.log(xtag.query(this, "aside")[0]);
-                if (this.hasAttribute("open")) {
-                    this.removeAttribute("open");
-                } else {
-                    this.setAttribute("open", "");
-                }
-            }
-        }
-    });
-})();
-
-(function() {
     var transform = xtag.prefix.js + "Transform";
     function getState(el) {
         var selected = xtag.query(el, "x-slides > x-slide[selected]")[0] || 0;
