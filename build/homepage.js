65119ab4abbff243e3524a3ba3cc693a99c7d165
Matt Claypotch <thepotch@gmail.com>
2014-06-12 13:33:14 -0700

    tabula rasa

--- build/homepage.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-var site = require('./statictools');
-
-console.log('generating homepage');
-site.staticPage(path.join('build','templates','homepage.html'), 'index.html');
-
